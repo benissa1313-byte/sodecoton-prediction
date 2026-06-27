8eac1083bf2d5b102dc195289623b80593790396
benissa1313-byte <ben.issa1313@gmail.com>
2026-06-27 18:34:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import seaborn as sns
